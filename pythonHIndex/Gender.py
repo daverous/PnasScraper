bb4732819274dc2685ee1d35e4d7b6ee7c1250ce
David Wilson <david.wilson@roslin.ed.ac.uk>
2018-10-10 13:22:48 +0100

    added further probabilities

--- pythonHIndex/Gender.py
+++ pythonHIndex/Gender.py
@@ -1,5 +1,6 @@
 from scopus import ScopusAuthor
 import pandas as pd
+from chicksexer import predict_gender
 # https://scopus.readthedocs.io/en/latest/reference/scopus.ScopusSearch.html?highlight=scopussearch
 from scopus import ScopusSearch
 from scopus import ScopusAbstract
@@ -12,14 +13,18 @@ data = pd.read_csv('out.csv')
 data.drop(data.columns[1:14], axis=1, inplace=True)
 
 genders = []
+gendersProbability = []
 d = gender.Detector()
 # remove first 14 cols pandas
 for i, row in data.iterrows():
 
     if 'firstAuthor' in row:
+        name = str(row['firstAuthor'])
+        name = name.encode("ascii", errors="ignore").decode()
         firstName = str(row['firstAuthor']).split(" ")[0]
         genders.append(d.get_gender(firstName))
-
+        gendersProbability.append(predict_gender(name))
 data['genders'] = genders
+data['gendersProbability'] = gendersProbability
 
 data.to_csv("genders.csv")
